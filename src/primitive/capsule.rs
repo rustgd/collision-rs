20290e4e4e39427a718493531f380cf66fb7c47d
Simon Rönnberg <seamonr@gmail.com>
2017-12-29 21:47:00 +0100

    Add documentation and unit tests for cylinder and capsule

--- src/primitive/capsule.rs
+++ src/primitive/capsule.rs
@@ -5,6 +5,7 @@ use {Aabb3, Ray3};
 use prelude::*;
 
 /// Capsule primitive
+/// Capsule body is aligned with the Y axis, with local origin in the center of the capsule.
 #[derive(Debug, Clone, PartialEq)]
 #[cfg_attr(feature = "eders", derive(Serialize, Deserialize))]
 pub struct Capsule<S> {
@@ -160,12 +161,6 @@ where
             t1.min(t2)
         };
 
-        let pc = r.origin + r.direction * t;
-        let full_half_height = self.half_height + self.radius;
-        if (pc.y > full_half_height) || (pc.y < -full_half_height) {
-            return None;
-        }
-
         // top sphere
         let l = Vector3::new(-r.origin.x, self.half_height - r.origin.y, -r.origin.z);
         let tca = l.dot(r.direction);
@@ -174,7 +169,7 @@ where
             if d2 <= self.radius * self.radius {
                 let thc = (self.radius * self.radius - d2).sqrt();
                 let t0 = tca - thc;
-                if t0 >= S::zero() && t0 < t {
+                if t0 >= S::zero() && (t.is_nan() || t0 < t) {
                     t = t0;
                 }
             }
@@ -188,12 +183,189 @@ where
             if d2 <= self.radius * self.radius {
                 let thc = (self.radius * self.radius - d2).sqrt();
                 let t0 = tca - thc;
-                if t0 >= S::zero() && t0 < t {
+                if t0 >= S::zero() && (t.is_nan() || t0 < t) {
                     t = t0;
                 }
             }
         }
 
+        if t.is_nan() {
+            return None;
+        }
+
+        let pc = r.origin + r.direction * t;
+        let full_half_height = self.half_height + self.radius;
+        if (pc.y > full_half_height) || (pc.y < -full_half_height) {
+            return None;
+        }
+
         Some(r.origin + r.direction * t)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std;
+
+    use cgmath::{Decomposed, Quaternion, Rad, Vector3};
+
+    use super::*;
+
+    #[test]
+    fn test_capsule_aabb() {
+        let capsule = Capsule::new(2., 1.);
+        assert_eq!(
+            Aabb3::new(Point3::new(-1., -3., -1.), Point3::new(1., 3., 1.)),
+            capsule.get_bound()
+        );
+    }
+
+    #[test]
+    fn test_capsule_support_1() {
+        let capsule = Capsule::new(2., 1.);
+        let direction = Vector3::new(1., 0., 0.);
+        let transform = transform(0., 0., 0., 0.);
+        let point = capsule.support_point(&direction, &transform);
+        assert_ulps_eq!(Point3::new(1., 2., 0.), point);
+    }
+
+    #[test]
+    fn test_capsule_support_2() {
+        let capsule = Capsule::new(2., 1.);
+        let direction = Vector3::new(0.5, -1., 0.).normalize();
+        let transform = transform(0., 0., 0., 0.);
+        let point = capsule.support_point(&direction, &transform);
+        assert_ulps_eq!(Point3::new(0.44721365, -2.8944273, 0.), point);
+    }
+
+    #[test]
+    fn test_capsule_support_3() {
+        let capsule = Capsule::new(2., 1.);
+        let direction = Vector3::new(0., 1., 0.);
+        let transform = transform(0., 0., 0., 0.);
+        let point = capsule.support_point(&direction, &transform);
+        assert_ulps_eq!(Point3::new(0., 3., 0.), point);
+    }
+
+    #[test]
+    fn test_capsule_support_4() {
+        let capsule = Capsule::new(2., 1.);
+        let direction = Vector3::new(1., 0., 0.);
+        let transform = transform(10., 0., 0., 0.);
+        let point = capsule.support_point(&direction, &transform);
+        assert_ulps_eq!(Point3::new(11., 2., 0.), point);
+    }
+
+    #[test]
+    fn test_capsule_support_5() {
+        let capsule = Capsule::new(2., 1.);
+        let direction = Vector3::new(1., 0., 0.);
+        let transform = transform(0., 0., 0., std::f32::consts::PI);
+        let point = capsule.support_point(&direction, &transform);
+        assert_ulps_eq!(Point3::new(1., -2., 0.), point);
+    }
+
+    #[test]
+    fn test_discrete_1() {
+        let capsule = Capsule::new(2., 1.);
+        let ray = Ray3::new(Point3::new(-3., 0., 0.), Vector3::new(1., 0., 0.));
+        assert!(capsule.intersects(&ray));
+    }
+
+    #[test]
+    fn test_discrete_2() {
+        let capsule = Capsule::new(2., 1.);
+        let ray = Ray3::new(Point3::new(-3., 0., 0.), Vector3::new(-1., 0., 0.));
+        assert!(!capsule.intersects(&ray));
+    }
+
+    #[test]
+    fn test_discrete_3() {
+        let capsule = Capsule::new(2., 1.);
+        let ray = Ray3::new(
+            Point3::new(0., 3., 0.),
+            Vector3::new(0.1, -1., 0.1).normalize(),
+        );
+        assert!(capsule.intersects(&ray));
+    }
+
+    #[test]
+    fn test_discrete_4() {
+        let capsule = Capsule::new(2., 1.);
+        let ray = Ray3::new(
+            Point3::new(0., 3., 0.),
+            Vector3::new(0.1, 1., 0.1).normalize(),
+        );
+        assert!(!capsule.intersects(&ray));
+    }
+
+    #[test]
+    fn test_discrete_5() {
+        let capsule = Capsule::new(2., 1.);
+        let ray = Ray3::new(
+            Point3::new(0., 3., 0.),
+            Vector3::new(0., -1., 0.).normalize(),
+        );
+        assert!(capsule.intersects(&ray));
+    }
+
+    #[test]
+    fn test_continuous_1() {
+        let capsule = Capsule::new(2., 1.);
+        let ray = Ray3::new(Point3::new(-3., 0., 0.), Vector3::new(1., 0., 0.));
+        assert_eq!(Some(Point3::new(-1., 0., 0.)), capsule.intersection(&ray));
+    }
+
+    #[test]
+    fn test_continuous_2() {
+        let capsule = Capsule::new(2., 1.);
+        let ray = Ray3::new(Point3::new(-3., 0., 0.), Vector3::new(-1., 0., 0.));
+        assert_eq!(None, capsule.intersection(&ray));
+    }
+
+    #[test]
+    fn test_continuous_3() {
+        let capsule = Capsule::new(2., 1.);
+        let ray = Ray3::new(
+            Point3::new(0., 4., 0.),
+            Vector3::new(0.1, -1., 0.1).normalize(),
+        );
+        assert_eq!(
+            Some(Point3::new(
+                0.10102588514869944,
+                2.989741148513006,
+                0.10102588514869944
+            )),
+            capsule.intersection(&ray)
+        );
+    }
+
+    #[test]
+    fn test_continuous_4() {
+        let capsule = Capsule::new(2., 1.);
+        let ray = Ray3::new(
+            Point3::new(0., 3., 0.),
+            Vector3::new(0.1, 1., 0.1).normalize(),
+        );
+        assert_eq!(None, capsule.intersection(&ray));
+    }
+
+    #[test]
+    fn test_continuous_5() {
+        let capsule = Capsule::new(2., 1.);
+        let ray = Ray3::new(
+            Point3::new(0., 4., 0.),
+            Vector3::new(0., -1., 0.).normalize(),
+        );
+        assert_eq!(Some(Point3::new(0., 3., 0.)), capsule.intersection(&ray));
+    }
+
+    // util
+    fn transform(dx: f32, dy: f32, dz: f32, rot: f32) -> Decomposed<Vector3<f32>, Quaternion<f32>> {
+        Decomposed {
+            scale: 1.,
+            rot: Quaternion::from_angle_z(Rad(rot)),
+            disp: Vector3::new(dx, dy, dz),
+        }
     }
 }
--- src/primitive/cylinder.rs
+++ src/primitive/cylinder.rs
@@ -5,6 +5,7 @@ use {Aabb3, Ray3};
 use prelude::*;
 
 /// Cylinder primitive
+/// Cylinder body is aligned with the Y axis, with local origin in the center of the cylinders.
 #[derive(Debug, Clone, PartialEq)]
 #[cfg_attr(feature = "eders", derive(Serialize, Deserialize))]
 pub struct Cylinder<S> {
@@ -41,11 +42,15 @@ where
         let negative = result.y.is_sign_negative();
 
         result.y = S::zero();
-        result = result.normalize();
-        if result.is_zero() {
-            result = Zero::zero(); // cancel out any inconsistencies
+        if result.magnitude2().is_zero() {
+            result = Zero::zero();
         } else {
-            result = result * self.radius;
+            result = result.normalize();
+            if result.is_zero() {
+                result = Zero::zero(); // cancel out any inconsistencies
+            } else {
+                result = result * self.radius;
+            }
         }
         if negative {
             result.y = -self.half_height;
@@ -81,8 +86,18 @@ where
         let b = two * (r.direction.x * r.origin.x + r.direction.z * r.origin.z);
         let c = r.origin.x * r.origin.x + r.origin.z * r.origin.z - self.radius * self.radius;
 
+        // check if a is zero -> vector is parallel with y axis
+        if a.is_zero() {
+            if r.direction.y.is_zero() {
+                return false;
+            }
+
+            return (r.origin.y >= -self.half_height && r.direction.y <= S::zero())
+                || (r.origin.y <= self.half_height && r.direction.y >= S::zero());
+        }
         let four = two + two;
         let dr = b * b - four * a * c;
+        println!("{:?}", dr);
         if dr < S::zero() {
             return false;
         }
@@ -90,6 +105,8 @@ where
         let t1 = (-b + drsqrt) / (two * a);
         let t2 = (-b - drsqrt) / (two * a);
 
+        println!("{:?}", t1);
+        println!("{:?}", t2);
         if t1 < S::zero() && t2 < S::zero() {
             return false;
         }
@@ -103,7 +120,29 @@ where
         };
 
         let pc = r.origin + r.direction * t;
-        pc.y <= self.half_height && pc.y >= -self.half_height
+        if pc.y <= self.half_height && pc.y >= -self.half_height {
+            return true;
+        }
+
+        let n = -Vector3::unit_y();
+        let tp = -(self.half_height + r.origin.dot(n)) / r.direction.dot(n);
+        if tp >= S::zero() {
+            let p = r.origin + r.direction * tp;
+            if p.x * p.x + p.z * p.z < self.radius * self.radius {
+                return true;
+            }
+        }
+
+        let n = Vector3::unit_y();
+        let tb = -(-self.half_height + r.origin.dot(n)) / r.direction.dot(n);
+        if tb >= S::zero() {
+            let p = r.origin + r.direction * tb;
+            if p.x * p.x + p.z * p.z < self.radius * self.radius {
+                return true;
+            }
+        }
+
+        false
     }
 }
 
@@ -119,6 +158,27 @@ where
         let a = r.direction.x * r.direction.x + r.direction.z * r.direction.z;
         let b = two * (r.direction.x * r.origin.x + r.direction.z * r.origin.z);
         let c = r.origin.x * r.origin.x + r.origin.z * r.origin.z - self.radius * self.radius;
+
+        if a.is_zero() {
+            if r.direction.y.is_zero() {
+                return None;
+            }
+
+            if r.origin.y >= self.half_height && r.direction.y < S::zero() {
+                return Some(Point3::new(S::zero(), self.half_height, S::zero()));
+            }
+            if r.origin.y >= -self.half_height && r.direction.y < S::zero() {
+                return Some(Point3::new(S::zero(), -self.half_height, S::zero()));
+            }
+            if r.origin.y <= -self.half_height && r.direction.y > S::zero() {
+                return Some(Point3::new(S::zero(), -self.half_height, S::zero()));
+            }
+            if r.origin.y <= self.half_height && r.direction.y > S::zero() {
+                return Some(Point3::new(S::zero(), self.half_height, S::zero()));
+            }
+
+            return None;
+        }
 
         let four = two + two;
         let dr = b * b - four * a * c;
@@ -141,11 +201,6 @@ where
             t1.min(t2)
         };
 
-        let pc = r.origin + r.direction * t;
-        if (pc.y > self.half_height) || (pc.y < -self.half_height) {
-            return None;
-        }
-
         let n = -Vector3::unit_y();
         let tp = -(self.half_height + r.origin.dot(n)) / r.direction.dot(n);
         if tp >= S::zero() && tp < t {
@@ -164,6 +219,160 @@ where
             }
         }
 
+        let pc = r.origin + r.direction * t;
+        if (pc.y > self.half_height) || (pc.y < -self.half_height) {
+            return None;
+        }
         Some(r.origin + r.direction * t)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std;
+
+    use cgmath::{Decomposed, Quaternion, Rad, Vector3};
+
+    use super::*;
+
+    #[test]
+    fn test_cylinder_aabb() {
+        let cylinder = Cylinder::new(2., 1.);
+        assert_eq!(
+            Aabb3::new(Point3::new(-1., -2., -1.), Point3::new(1., 2., 1.)),
+            cylinder.get_bound()
+        );
+    }
+
+    #[test]
+    fn test_cylinder_support_1() {
+        let cylinder = Cylinder::new(2., 1.);
+        let direction = Vector3::new(1., 0., 0.);
+        let transform = transform(0., 0., 0., 0.);
+        let point = cylinder.support_point(&direction, &transform);
+        assert_ulps_eq!(Point3::new(1., 2., 0.), point);
+    }
+
+    #[test]
+    fn test_cylinder_support_2() {
+        let cylinder = Cylinder::new(2., 1.);
+        let direction = Vector3::new(0.5, -1., 0.).normalize();
+        let transform = transform(0., 0., 0., 0.);
+        let point = cylinder.support_point(&direction, &transform);
+        assert_ulps_eq!(Point3::new(1., -2., 0.), point);
+    }
+
+    #[test]
+    fn test_cylinder_support_3() {
+        let cylinder = Cylinder::new(2., 1.);
+        let direction = Vector3::new(0., 1., 0.);
+        let transform = transform(0., 0., 0., 0.);
+        let point = cylinder.support_point(&direction, &transform);
+        assert_ulps_eq!(Point3::new(0., 2., 0.), point);
+    }
+
+    #[test]
+    fn test_cylinder_support_4() {
+        let cylinder = Cylinder::new(2., 1.);
+        let direction = Vector3::new(1., 0., 0.);
+        let transform = transform(10., 0., 0., 0.);
+        let point = cylinder.support_point(&direction, &transform);
+        assert_ulps_eq!(Point3::new(11., 2., 0.), point);
+    }
+
+    #[test]
+    fn test_cylinder_support_5() {
+        let cylinder = Cylinder::new(2., 1.);
+        let direction = Vector3::new(1., 0., 0.);
+        let transform = transform(0., 0., 0., std::f32::consts::PI);
+        let point = cylinder.support_point(&direction, &transform);
+        assert_ulps_eq!(Point3::new(1., -2., 0.), point);
+    }
+
+    #[test]
+    fn test_discrete_1() {
+        let cylinder = Cylinder::new(2., 1.);
+        let ray = Ray3::new(Point3::new(-3., 0., 0.), Vector3::new(1., 0., 0.));
+        assert!(cylinder.intersects(&ray));
+    }
+
+    #[test]
+    fn test_discrete_2() {
+        let cylinder = Cylinder::new(2., 1.);
+        let ray = Ray3::new(Point3::new(-3., 0., 0.), Vector3::new(-1., 0., 0.));
+        assert!(!cylinder.intersects(&ray));
+    }
+
+    #[test]
+    fn test_discrete_3() {
+        let cylinder = Cylinder::new(2., 1.);
+        let ray = Ray3::new(
+            Point3::new(0., 3., 0.),
+            Vector3::new(0.1, -1., 0.1).normalize(),
+        );
+        assert!(cylinder.intersects(&ray));
+    }
+
+    #[test]
+    fn test_discrete_4() {
+        let cylinder = Cylinder::new(2., 1.);
+        let ray = Ray3::new(
+            Point3::new(0., 3., 0.),
+            Vector3::new(0.1, 1., 0.1).normalize(),
+        );
+        assert!(!cylinder.intersects(&ray));
+    }
+
+    #[test]
+    fn test_continuous_1() {
+        let cylinder = Cylinder::new(2., 1.);
+        let ray = Ray3::new(Point3::new(-3., 0., 0.), Vector3::new(1., 0., 0.));
+        assert_eq!(Some(Point3::new(-1., 0., 0.)), cylinder.intersection(&ray));
+    }
+
+    #[test]
+    fn test_continuous_2() {
+        let cylinder = Cylinder::new(2., 1.);
+        let ray = Ray3::new(Point3::new(-3., 0., 0.), Vector3::new(-1., 0., 0.));
+        assert_eq!(None, cylinder.intersection(&ray));
+    }
+
+    #[test]
+    fn test_continuous_3() {
+        let cylinder = Cylinder::new(2., 1.);
+        let ray = Ray3::new(
+            Point3::new(0., 3., 0.),
+            Vector3::new(0.1, -1., 0.1).normalize(),
+        );
+        assert_eq!(Some(Point3::new(0.1, 2., 0.1)), cylinder.intersection(&ray));
+    }
+
+    #[test]
+    fn test_continuous_4() {
+        let cylinder = Cylinder::new(2., 1.);
+        let ray = Ray3::new(
+            Point3::new(0., 3., 0.),
+            Vector3::new(0.1, 1., 0.1).normalize(),
+        );
+        assert_eq!(None, cylinder.intersection(&ray));
+    }
+
+    #[test]
+    fn test_continuous_5() {
+        let cylinder = Cylinder::new(2., 1.);
+        let ray = Ray3::new(
+            Point3::new(0., 3., 0.),
+            Vector3::new(0., -1., 0.).normalize(),
+        );
+        assert_eq!(Some(Point3::new(0., 2., 0.)), cylinder.intersection(&ray));
+    }
+
+    // util
+    fn transform(dx: f32, dy: f32, dz: f32, rot: f32) -> Decomposed<Vector3<f32>, Quaternion<f32>> {
+        Decomposed {
+            scale: 1.,
+            rot: Quaternion::from_angle_z(Rad(rot)),
+            disp: Vector3::new(dx, dy, dz),
+        }
     }
 }
